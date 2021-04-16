b1d61ca05f0f7aff0da79f7c6f5059c2c1f72583
wangkun <15380314863@163.com>
2021-04-16 11:27:53 +0800

    基础页面构建

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            <a href="#" target="_blank">Summer</a> 欢迎访问 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="#">联系我们</a></p>
+    </div>
+</footer>

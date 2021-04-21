8b282eb8b92cf961b3048c6c926c31e030d0d35c
wangkun <15380314863@163.com>
2021-04-21 14:31:54 +0800

    强制用户认证邮箱

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use HasFactory, Notifiable;
-
+    use HasFactory, Notifiable, MustVerifyEmailTrait;
     /**
      * The attributes that are mass assignable.
      *

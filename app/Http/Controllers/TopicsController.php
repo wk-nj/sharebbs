3cacc1cafff5279d553ebcfd85aad14916cce7ff
wangkun <15380314863@163.com>
2021-04-22 17:53:43 +0800

    话题排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,9 +14,9 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request)
 	{
-		$topics = Topic::with(['category', 'user'])->paginate();
+		$topics = Topic::withOrder($request->order)->with(['category', 'user'])->paginate();
 		return view('topics.index', compact('topics'));
 	}
 

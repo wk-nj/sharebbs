f53bad3484d1f193efe3f3bec3e1908bbaca412a
wangkun <15380314863@163.com>
2021-04-27 18:29:56 +0800

    站点权限部署

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Telescope\Telescope;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Telescope::auth(function ($request) {
+            return \Auth::user()->hasRole('Founder');
+        });
     }
 }
